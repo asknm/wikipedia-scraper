0a62935f97d0e3b505076ca08171b3262583e4c1
Ask Norheim Morken <asknm@stud.ntnu.no>
2019-10-16 15:22:55 +0200

    Basic counter

---
+++ counter.py
@@ -0,0 +1,34 @@
+class counter:
+    def start(self):
+        file = open("completed_with_path.txt", "r")
+        pages = []
+        for line in file:
+            page = line.split()[0]
+            index = self.in_pages(pages, page)
+            if index > -1:
+                #pages[index][1] += 1
+                pages.append((page, pages[index][1] + 1))
+                pages.pop(index)
+            else:
+                pages.append((page, 1))
+        pages.sort(key=lambda tup: tup[1])
+        file = open("top_pages.txt", "w")
+        for p in pages:
+            file.write(p[0] + ' ' + str(p[1]) + '\n')
+
+
+    def in_pages(self, pages, page):
+        i = 0
+        for p in pages:
+            if page == p[0]:
+                return i
+            i += 1
+        return -1
+
+def main():
+    c = counter()
+    c.start()
+
+
+if __name__ == '__main__':
+    main()
---
+++ path_scraper.py
@@ -0,0 +1,100 @@
+import scrapy
+
+
+class WikiSpider(scrapy.Spider):
+    name = "wiki_spider"
+    custom_settings = {
+        'DUPEFILTER_DEBUG': True,
+    }
+    file = open("to_visit.txt", "r")
+    start = file.readline().split('\n')[0]
+    start_urls = ["https://en.wikipedia.org" + start]
+    visited = [start]
+
+    def parse(self, response):
+        #ps = response.css('div.mw-parser-output > pre').extract()
+        ps = response.css('div.mw-parser-output > p').extract()
+        in_parentheses = 0
+        link = None
+        done = False
+        for p in ps:
+            for word in p.split():
+                in_parentheses += len(word.split('(')) - 1
+                in_parentheses -= len(word.split(')')) - 1
+                in_parentheses += len(word.split('[')) - 1
+                in_parentheses -= len(word.split(']')) - 1
+                if in_parentheses == 0 and word.startswith('href='):
+                    link = word.split('"')[-2]
+                    if link.startswith("/wiki/") and len(link.split('/')) == 3 and len(link.split('#')) == 1 \
+                            and not link == '/wiki/Geographic_coordinate_system':
+                        done = True
+                        break
+            if done:
+                break
+        print(link)
+        if link is None:
+            self.set_visited(False)
+        elif link == '/wiki/Philosophy':
+            self.visited.append(link)
+            self.set_visited(True)
+        elif link in self.visited:
+            print(self.visited)
+            self.visited.append(link)
+            self.set_visited(False)
+        elif self.in_completed(link) > -1:
+            self.visited.append(link)
+            index = self.in_completed(link)
+            value = ""
+            with open("completed_with_path.txt") as fp:
+                for i, line in enumerate(fp):
+                    if i == index:
+                        value = line.split()[-1]
+                    elif i > index:
+                        break
+            if value == "True":
+                self.set_visited(True)
+            elif value == "False":
+                self.set_visited(False)
+            else:
+                print("SOMETHING WENT WRONG!")
+        else:
+            self.visited.append(link)
+            yield response.follow(link, callback=self.parse)
+            return
+
+        file = open("to_visit.txt", "r")
+        for line in file:
+            if self.in_completed(line) == -1:
+                next = line[:-1]
+                self.visited.append(next)
+                # print('s')
+                # print(len(self.to_visit))
+                yield response.follow(next, callback=self.parse)
+                break
+
+
+    def set_visited(self, value):
+        file = open("completed_with_path.txt", "a")
+        for i in range(len(self.visited)):
+            file.write(self.visited[i] + " " + str(self.visited) + " " + str(value) + "\n")
+        file.close()
+        file = open("to_visit.txt", "r")
+        to_visit = file.readlines()
+        file.close()
+        for v in self.visited:
+            for line in to_visit:
+                if line.startswith(v):
+                    to_visit.remove(line)
+        file = open("to_visit.txt", "w")
+        file.writelines(to_visit)
+        file.close()
+        self.visited = []
+
+    def in_completed(self, link):
+        file = open("completed_with_path.txt", "r")
+        i = 0
+        for line in file:
+            if line.startswith(link):
+                return i
+            i += 1
+        return -1
